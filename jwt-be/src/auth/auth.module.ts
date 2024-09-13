c3b676cc102f9659032b053c3d9ca4ff75d87125
Hong-Jun-Il <86016726+Hong-Jun-Il@users.noreply.github.com>
2024-09-13 16:02:42 +0900

    save

--- jwt-be/src/auth/auth.module.ts
+++ jwt-be/src/auth/auth.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: 'abc123',
+    }),
+  ],
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
 })
 export class AuthModule {}
