ace1c40edda3a9a6ff865c2dab4135b8b1664e8a
Hong-Jun-Il <86016726+Hong-Jun-Il@users.noreply.github.com>
2024-11-27 01:41:07 +0900

    save

--- jwt-fe/src/app/(beforeLogin)/posts/_components/PostList.tsx
+++ jwt-fe/src/app/(beforeLogin)/posts/_components/PostList.tsx
@@ -1,9 +1,24 @@
 "use client";
 
 import { useGetPosts } from "@/hooks/queries";
+import style from "./postList.module.scss";
+import Post from "../../_components/Post";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function PostList() {
-  const test = useGetPosts();
-  console.log(test);
-  return <div>PostList</div>;
+  const { data, isLoading } = useGetPosts();
+
+  return (
+    <>
+      <ul className={style.postListWrapper}>
+        <Skeleton className="h-[50px] w-[120px]" />
+        {/* {isLoading ? (
+          <Skeleton />
+        ) : (
+          <>{data?.pages.flat(2).map((e) => <Post key={e.id} data={e} />)}</>
+        )} */}
+      </ul>
+      <div />
+    </>
+  );
 }
--- jwt-fe/src/hooks/queries.ts
+++ jwt-fe/src/hooks/queries.ts
@@ -1,9 +1,14 @@
+import { PostType } from "@/types/PostType";
 import { useInfiniteQuery } from "@tanstack/react-query";
 
 export function useGetPosts() {
   return useInfiniteQuery({
     queryKey: ["posts"],
-    queryFn: async ({ pageParam }: { pageParam: number }) => {
+    queryFn: async ({
+      pageParam,
+    }: {
+      pageParam: number;
+    }): Promise<PostType[]> => {
       const res = await fetch(
         `${process.env.NEXT_PUBLIC_BASE_URL}/posts?page=${pageParam}`,
       );
@@ -23,4 +28,14 @@ export function useGetPosts() {
       return lastPageParam + 1;
     },
   });
+}
+
+export async function getAnalitics() {
+  const res = await fetch("http://localhost:5173/analysis");
+
+  if (!res.ok) {
+    throw new Error("HTTP ERROR");
+  }
+
+  return res.json();
 }
